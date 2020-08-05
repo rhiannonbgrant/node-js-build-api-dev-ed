bdd5aa68e549fa2999822d5eff51918b780d22d7
Rhiannon Grant <rbgrant87@gmail.com>
2020-08-05 15:55:11 +1000

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+dotenv.config();
+
+//Import Routes
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("connected to DB");
+});
+
+//How do we start listening to the server
+app.listen(3000);
